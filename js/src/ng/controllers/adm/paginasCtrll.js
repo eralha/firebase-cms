cd5d536e52e0d04d2b8fef09941211e59bb6a365
Emanuel Ralha <emaralha@yahoo.com>
2015-07-15 09:45:19 +0100

    Adding pages state view

--- js/src/ng/controllers/adm/paginasCtrll.js
+++ js/src/ng/controllers/adm/paginasCtrll.js
@@ -2,6 +2,8 @@ define(['app'], function(app){
 
 	app.controller('paginasCtrll', ['$scope', '$firebaseArray', 'Slug', 'configService', function($scope, $firebaseArray, Slug, configService){
 
+		$scope.state = 'available';
+
 		configService.load().then(function(appConfig){
             var paginasRef = new Firebase(appConfig.paginasRef);
             	$scope.paginas = $firebaseArray(paginasRef);
@@ -17,6 +19,10 @@ define(['app'], function(app){
 			$scope.sendToTrash = function(page){
 				page.state = 'trashed';
 				$scope.paginas.$save(page).then(function(ref) {});
+			}
+
+			$scope.filterPages = function(state){
+				$scope.state = state;
 			}
 
     }]);
